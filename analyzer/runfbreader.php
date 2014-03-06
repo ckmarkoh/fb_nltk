cd2dd1683e2dd41f1b63256e271b89b14cd2c568
ckmarkoh <mark23456@hotmail.com>
2014-03-06 23:17:28 +0800

    bug in runfbreader.php

--- analyzer/runfbreader.php
+++ analyzer/runfbreader.php
@@ -1,19 +1,27 @@
 <?php
+function divide($x,$y){
+    if ($y!=0){
+        return $x/$y;
+    }
+    else{
+        return 0;
+    }
 
-$uid=$_POST['uid'];
-$access_token=$_POST['access_token'];
-#$uid='1037998582';
-#$access_token='CAAFbxFk1h5QBANvdl6NYxcAZCQxCGKh6WPWQZCVz3ZC0POjW3HZBE0u8WFMVMSPbIbDpxtwPnop3A8KDArhjhukd1D9c0fo8vh32YahXaieBYDkfnw2w2h3YBnACUZAlO507TUaqz5VpjaKQVOLAllziRr9NfdFZCGE82EXKnBy61k4G4avxny';
+}
+#$uid=$_POST['uid'];
+#$access_token=$_POST['access_token'];
+$uid='1233266413';
+$access_token='CAAUpDilvHqEBAK6UWNUAZAVFCRyZADGFhfnKGhZBxz507qyHAcUt7VFpKgxE9VFjOwaVZBqCew33NP7TLb6RP9ZAV61nGmXl5M853BBP5yRToCFVWabhIibWyC1K1JZCK4YWcyA4yxJaKRFobhzTl9VR4PEfL3ZBC05h3npDTc4ZAPfGqz8t02GaRDtDaoj4gqWgSbEecZA3ZAJwZDZD';
 #echo $uid.'<br/>'.$access_token.'<br/>';
 
 $result_2="";
 $result_1= shell_exec('python meifbcrawler.py '.$uid.' '.$access_token);
-//print_r($result_1);
+print_r($result_1);
 
-if( count($result_1) > 0 ){
-    $result_21= shell_exec('python fb_info_type.py '.$uid);
-    $result_22= shell_exec('python fb_post_type.py '.$uid);
-}
+//if( count($result_1) > 0 ){
+$result_21= shell_exec('python fb_info_type.py '.$uid);
+$result_22= shell_exec('python fb_post_type.py '.$uid);
+//}
 
 $key=array("EXT","AGR","CON","NEO","OPE");
 
@@ -25,12 +33,12 @@ $vs2=array_sum($v2);
 
 $v3=array();
 foreach($key as $k ){
-    $v3[$k]=$v1[$k]/$vs1+$v2[$k]/$vs2;
+    $v3[$k]=divide($v1[$k],$vs1)+divide($v2[$k],$vs2);
 }
 $vs3=array_sum($v3);
 
 foreach($key as $k ){
-    $v3[$k]=$v3[$k]/$vs3;
+    $v3[$k]=divide($v3[$k],$vs3);
 }
 echo json_encode($v3);
 
